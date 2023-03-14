735639cc0415c8a5fe2dece5445ea864524dc3ba
max <bozzhik@ya.ru>
2023-03-14 17:54:26 +0300

    optimize script.py for production

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
 import json
 import fileinput
+import shutil
 
+# Define the source and destination file paths
+source_file = 'index.html'
+destination_file = 'build/code.html'
+
+# Copy the source file to the destination folder
+shutil.copyfile(source_file, destination_file)
+
+# Generate the HTML code from JSON data
 with open('data.json') as f:
     data = json.load(f)
 
@@ -15,7 +24,8 @@ for d in data:
     </a>
     """
 
-for line in fileinput.input('shortcuts.html', inplace=True):
+# Replace the target element with the generated HTML code in the destination file
+for line in fileinput.input(destination_file, inplace=True):
     if 'id="PYTHON_REPLACE"' in line:
         print(html)
     else:
